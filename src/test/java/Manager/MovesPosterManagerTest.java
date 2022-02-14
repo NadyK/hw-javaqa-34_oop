97ad7c9b4251a51008227048f46cc8cf935b7f6e
Naan <naanko2021@gmail.com>
2022-02-14 14:00:58 +0300

    Reformat code

--- src/test/java/Manager/MovesPosterManagerTest.java
+++ src/test/java/Manager/MovesPosterManagerTest.java
@@ -1,6 +1,6 @@
 package Manager;
 
-import Domain.MoviesPoster;
+import domain.MoviesPoster;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,40 +8,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class MovesPosterManagerTest {
 
     @Test
-    public void shouldGetAll(){
+    public void shouldGetAll() {
         MovesPosterManager manager = new MovesPosterManager();
-        MoviesPoster first = new MoviesPoster(1,"first","dram",2021);
-        MoviesPoster second = new MoviesPoster(2,"second","сom",2019);
-        MoviesPoster third = new MoviesPoster(3,"third","adv",2022);
+        MoviesPoster first = new MoviesPoster(1, "first", "dram", 2021);
+        MoviesPoster second = new MoviesPoster(2, "second", "сom", 2019);
+        MoviesPoster third = new MoviesPoster(3, "third", "adv", 2022);
 
         manager.add(first);
         manager.add(second);
         manager.add(third);
 
-        MoviesPoster[] actual=manager.getAll();
-        MoviesPoster[] expected= new MoviesPoster[]{third, second, first};
+        MoviesPoster[] actual = manager.getAll();
+        MoviesPoster[] expected = new MoviesPoster[]{third, second, first};
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldGet2(){
+    public void shouldGet2() {
         MovesPosterManager manager = new MovesPosterManager();
-        MoviesPoster first = new MoviesPoster(1,"first","dram",2021);
-        MoviesPoster second = new MoviesPoster(2,"second","сom",2019);
-        MoviesPoster third = new MoviesPoster(3,"third","adv",2022);
+        MoviesPoster first = new MoviesPoster(1, "first", "dram", 2021);
+        MoviesPoster second = new MoviesPoster(2, "second", "сom", 2019);
+        MoviesPoster third = new MoviesPoster(3, "third", "adv", 2022);
 
         manager.add(first);
         manager.add(second);
         manager.add(third);
 
         manager.setLimitLength(2);
-        MoviesPoster[] actual=manager.getAll();
-        MoviesPoster[] expected= new MoviesPoster[]{third, second};
+        MoviesPoster[] actual = manager.getAll();
+        MoviesPoster[] expected = new MoviesPoster[]{third, second};
 
         assertArrayEquals(expected, actual);
     }
-
 
 
 }
